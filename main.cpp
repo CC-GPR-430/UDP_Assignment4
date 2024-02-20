4f2a064cf9f01a95783db6b69d5e5db5dbfc415d
QuincyG <quincy.gomescedeno@mymail.champlain.edu>
2024-02-19 22:26:14 -0500

    Working with classs code and added code to main

--- main.cpp
+++ main.cpp
@@ -10,39 +10,62 @@
 
 // Use this function in main() to try simple tests.
 int udp_assignment() {
-  {
-    std::cout << "Testing without request ids...\n";
-    UDPClient client(consts::HOST, consts::ECHO_PORT);
-    std::string result;
-    int error = client.send_message_by_character("Beautiful is better than ugly", result);
-    if (error == -1) {
-      std::cout << "Timed out.\n";
-      return -1;
-    } else {
-      std::cout << "Got result '" << result << "'.\n";
+    {
+        std::cout << "Testing without request ids...\n";
+        UDPClient client(consts::HOST, consts::ECHO_PORT);
+        std::string result;
+        int error = client.send_message_by_character("Beautiful is better than ugly", result);
+        if (error == -1) {
+            std::cout << "Timed out.\n";
+            return -1;
+        }
+        else {
+            std::cout << "Got result '" << result << "'.\n";
+        }
     }
-  }
 
-  {
-    std::cout << "Testing without request ids...\n";
-    UDPClient client(consts::HOST, consts::REQUEST_ID_PORT, true);
-    std::string result;
-    int error = client.send_message_by_character("To be or not to be, that is the question", result);
-    if (error == -1) {
-      std::cout << "Timed out.\n";
-      return -1;
-    } else {
-      std::cout << "Got result '" << result << "'.\n";
+    {
+        std::cout << "Testing without request ids...\n";
+        UDPClient client(consts::HOST, consts::REQUEST_ID_PORT, true);
+        std::string result;
+        int error = client.send_message_by_character("To be or not to be, that is the question", result);
+        if (error == -1) {
+            std::cout << "Timed out.\n";
+            return -1;
+        }
+        else {
+            std::cout << "Got result '" << result << "'.\n";
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
 
-int main(int argc, char *argv[]) {
-  /* TODO: SET UP / TEAR DOWN SOCKET LIBRARY HERE; SEED RANDOM
-     NUMBERS */
-  int result = run_all_tests();
+void generateRandomNumber()
+{
+    int ranNum = rand();
+    std::cout << "Number Selected: " << ranNum << "\n";
+}
 
-  return result;
+int main(int argc, char* argv[]) {
+    /* TODO: SET UP / TEAR DOWN SOCKET LIBRARY HERE; SEED RANDOM
+       NUMBERS */
+
+       //int result = run_all_tests();
+
+    SockLibInit();
+
+    // Do this ONCE per program:
+    srand(time(NULL));
+    generateRandomNumber();
+
+
+    // Run client if no args passed on command line;
+    // otherwise, run server.
+
+    int result = run_all_tests();
+
+    SockLibShutdown();
+
+    return result;
 }
--- udp_client.h
+++ udp_client.h
@@ -6,14 +6,85 @@
 
 class UDPClient {
 public:
-  UDPClient(const char* host, int port, bool include_ids = false)
-  {
-    /* TODO: IMPLEMENT THIS */
-  }
+	UDPClient(const char* host, int port, bool include_ids = false)
+	{
+		/* TODO: IMPLEMENT THIS */
+		Socket udp_sock(Socket::Family::INET, Socket::Type::DGRAM);
+		// udp_sock.SetTimeout(wait_time);
 
-  int send_message_by_character(const std::string& str, std::string& result)
-  {
-    /* TODO: IMPLEMENT THIS */
-    return -1;
-  }
+		// That's it actually
+		// Now we can send and recv :)
+
+		std::string msg_to_send("Hello, server! How are you?");
+		// Send to this computer, which "127.0.0.1" always refers to.
+		Address server_addr("127.0.0.1", 10245);
+
+
+		char buffer[4096];
+		Address msg_from;
+
+		// RecvFrom returns the number of bytes received
+		// and creates a new address, which it stores in the
+		// last parameter. This address is the address from
+		// which the data arrived.
+		int nbytes_recvd = -1;
+		// while (wait_time < max_wait_time && nbytes_recvd == -1)
+		{
+			// SendTo() takes an extra parameter as compared to Send():
+			// the address to which the data should be sent.
+			// The same socket can send data to any address, unlike
+			// with TCP, where a socket is bound a single connection.
+			size_t nbytes_sent = udp_sock.SendTo(msg_to_send.data(),
+				msg_to_send.size(),
+				server_addr);
+
+			std::cout << "Sent " << nbytes_sent << " bytes to "
+				<< server_addr << "\n";
+
+
+			// Problem: RecvFrom() receives data from *anyone* -- but
+			// we only want data that the server sent back! How can
+			// we make sure the data we got back is from the server?
+			// Some options:
+			//	1.	RecvFrom() tells us the address from which the message
+			//		came. Can compare that with the server's address
+			//		to see if the data came from the server.
+			int nbytes_recvd = udp_sock.RecvFrom(buffer, sizeof(buffer), msg_from);
+
+			// nbytes_recvd == -1 => We did not receive data for some reason
+			// nbytes_recvd  > 0  => We received nbytes_recvd bytes.
+			if (nbytes_recvd == -1)
+			{
+				// An error occurred in our receive!
+				if (udp_sock.GetLastError() == Socket::Error::SOCKLIB_ETIMEDOUT)
+				{
+					std::cout << "Timed out. Maybe retrying...\n";
+					// We timed out! Increase the timeout time and try again.
+					wait_time *= 2;
+					udp_sock.SetTimeout(wait_time);
+				}
+			}
+		}
+
+		if (wait_time > max_wait_time)
+		{
+			std::cout << "Never received a message!! Now I really AM sad :(\n";
+			return;
+		}
+
+		std::cout << "Received " << nbytes_recvd << " bytes from " << msg_from
+			<< ": '";
+		std::cout.write(buffer, nbytes_recvd);
+		std::cout << "'\n";
+
+	}
+
+	int send_message_by_character(const std::string& str, std::string& result)
+	{
+		/* TODO: IMPLEMENT THIS */
+		std::string msg_to_send(str);
+
+
+		return -1;
+	}
 };
