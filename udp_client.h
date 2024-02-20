e6527e59990a764e0830a29ba71c3b511ebc89eb
QuincyG <quincy.gomescedeno@mymail.champlain.edu>
2024-02-19 22:57:46 -0500

    working on message being sent

--- udp_client.h
+++ udp_client.h
@@ -5,35 +5,34 @@
 #include <sstream>
 
 class UDPClient {
-public:
+private:
+	char* address;
+	int port;
+	bool include_ids;
+
+public: 
 	UDPClient(const char* host, int port, bool include_ids = false)
 	{
-		/* TODO: IMPLEMENT THIS */
-		Socket udp_sock(Socket::Family::INET, Socket::Type::DGRAM);
-		// udp_sock.SetTimeout(wait_time);
+		this->port = port;//
+		this->address = host;//IP ex.123.56.78
+		this->include_ids = include_ids;//QA_2
+	}
 
-		// That's it actually
-		// Now we can send and recv :)
+	int send_message_by_character(const std::string& str, std::string& result)
+	{
+		Socket udp_sock(Socket::Family::INET, Socket::Type::DGRAM);//UDP is DGRAM
+		udp_sock.SetTimeout(INITIAL_TIMEOUT);
+
+		Address server_addr(address, port);
 
 		std::string msg_to_send("Hello, server! How are you?");
-		// Send to this computer, which "127.0.0.1" always refers to.
-		Address server_addr("127.0.0.1", 10245);
-
-
+		
 		char buffer[4096];
 		Address msg_from;
 
-		// RecvFrom returns the number of bytes received
-		// and creates a new address, which it stores in the
-		// last parameter. This address is the address from
-		// which the data arrived.
 		int nbytes_recvd = -1;
 		// while (wait_time < max_wait_time && nbytes_recvd == -1)
 		{
-			// SendTo() takes an extra parameter as compared to Send():
-			// the address to which the data should be sent.
-			// The same socket can send data to any address, unlike
-			// with TCP, where a socket is bound a single connection.
 			size_t nbytes_sent = udp_sock.SendTo(msg_to_send.data(),
 				msg_to_send.size(),
 				server_addr);
@@ -76,14 +75,6 @@ public:
 			<< ": '";
 		std::cout.write(buffer, nbytes_recvd);
 		std::cout << "'\n";
-
-	}
-
-	int send_message_by_character(const std::string& str, std::string& result)
-	{
-		/* TODO: IMPLEMENT THIS */
-		std::string msg_to_send(str);
-
 
 		return -1;
 	}
